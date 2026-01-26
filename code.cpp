f9a2f4793a8d57a1ebc5287e1c70e18c6bb5ff33
Safwan Shaikh <walterwhte112@gmail.com>
2026-01-26 23:19:31 +0530

    cycle detection in BFS and DFS

--- code.cpp
+++ code.cpp
@@ -7,14 +7,14 @@ using namespace std;
 class Graph{
 
 
-    int V;
+    int Vertices;
     list<int> *listOfIntegers;
 
-public:                                                          // I believe this is the constructor, no this is the actual constructor
-    Graph(int V){                                               // Number of vertices of graph
+public:                                                          // this is the actual constructor
+    Graph(int Vertices){                                               // Number of vertices of graph
 
-            this->V=V;
-            listOfIntegers=new list<int>[V];
+            this->Vertices=Vertices;
+            listOfIntegers=new list<int>[Vertices];
 
     }  
 
@@ -30,14 +30,14 @@ void BFS(){                                                          //O(V+E)
     queue<int> queue;
     queue.push(0);
 
-    vector<bool> visited(V,false);
+    vector<bool> visited(Vertices,false);
 
     visited[0]=true;
 
 
     while(queue.size() >0) {
 
-        int u =queue.front();   // u-v
+        int u =queue.front();                                           // u-v
         queue.pop();
 
         cout << u << " ";
@@ -55,7 +55,7 @@ cout << endl;
 }
 void dfsHelper(int u,vector<bool> &vis ){                             //O(V+E)
 
-    cout << u << " " ;
+    cout << u << " ";
     vis[u] =true;
 
     for (int v : listOfIntegers[u]){                                   //u -> v 
@@ -69,15 +69,15 @@ void dfsHelper(int u,vector<bool> &vis ){                             //O(V+E)
 }
 
 //DFS Traversal
-void DFS(){
-    int src = 0 ;                                                            //hardcoded value ,really curious how i can turn it into finding a src arbitrarily.
-    vector<bool> vis(V,false);
+void DFS(){ 
+    int src = 0 ;                                                                     //hardcoded value ,really curious how i can turn it into finding a src arbitrarily.
+    vector<bool> vis(Vertices,false);
 
     dfsHelper(src,vis);
 }
 
 
-//Cycle detectin in undirected graph
+//Cycle detectin in undirected graph using DFS
 bool isCycleUnderDSF(int src, int parent , vector<bool>&vis){                           //core logic of the cycle detection in DFS
             vis[src]=true;
             list<int> neighbours = listOfIntegers[src];
@@ -89,19 +89,19 @@ bool isCycleUnderDSF(int src, int parent , vector<bool>&vis){                   
                         return true;
                   }
                    
-                }else if (v !=parent){                    //if cycle exists then return true.
+                }else if (v !=parent){                                                    //if cycle exists then return true.
                     return true;
                 }
                 
             }
-            return false;                                       //found no cycle
+            return false;                                                                //found no cycle
             
 }
 //to get rid of passing parent and source again and again
 bool isCycle(){
-vector <bool> vis(V,false);                                                                   //V = number of vertices already has false values assigned.
+vector <bool> vis(Vertices,false);                                                                   //V = number of vertices already has false values assigned.
 
-for(int i=0;i<V;i++){
+for(int i=0;i<Vertices;i++){
     if(!vis[i]){
 
         isCycleUnderDSF(i,-1,vis);                                                  // i: source , -1 : parent of source , vis : visited array
@@ -110,24 +110,62 @@ for(int i=0;i<V;i++){
 }
 return false;
 }
+bool isCycleUnderBFS(int src, vector<bool> &vis){
+    queue<pair<int,int>> q;
 
+   vis[src]=true;
+    q.push({src,-1});
+
+    while(q.size()>0){
+     
+        int u=q.front().first;
+        int parentU=q.front().second;
+        q.pop();
+
+        list<int> neighbours = listOfIntegers[u];        //storing the listofintegers on my u.
+
+
+        for( int v : neighbours){
+            if(!vis[v]){
+                q.push({v,u});        //u-----------v
+                 vis[v]=true;
+            }else if(v!=parentU){    //if there exist an edge then we got a cycle
+                return true;
+            }               
+
+        }
+    }
+    return false;  //got no cycle
+
+}
+bool isCycleBFS(){          //using these method to ensure we cover the entire graph as the graphs might be disjoint and also to make the code look readable
+    vector<bool> visited(Vertices,false);
+    if(!visited[Vertices]){
+        if(isCycleUnderBFS(Vertices,visited)){
+            return true;
+        }
+    }
+}
 };
 
 int main(){
     Graph g(5);
 
     g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(0,3);
     g.addEdge(1,2);
-    g.addEdge(1,3);
-    g.addEdge(2,4);
+    g.addEdge(3,4);
     
-    cout << "BFS : " ;
-    g.BFS();
+    //cout << "BFS : " ;
+    //g.BFS();
 
-    cout << "DFS : ";
-    g.DFS();
+    //cout << "DFS : ";
+    //g.DFS();
 
-    g.isCycle() ? cout << "Cycle detected\n" : cout << "No Cycle detected\n";
+  //  g.isCycle() ? cout << "Cycle detected using DFS \n" : cout << "No Cycle detected\n";
+
+    g.isCycleBFS() ? cout << "Cycle detected using BFS \n" : cout << "No Cycles detected\n";
 
     return 0;
 }
