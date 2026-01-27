6902db34fef7fbb0a6a87149944dbcb5a2487a6e
Safwan Shaikh <walterwhte112@gmail.com>
2026-01-27 22:14:07 +0530

    minor tweaks

--- code.cpp
+++ code.cpp
@@ -76,84 +76,6 @@ void DFS(){
     dfsHelper(src,vis);
 }
 
-
-//Cycle detectin in undirected graph using DFS
-bool isCycleUnderDSF(int src, int parent , vector<bool>&vis){                           //core logic of the cycle detection in DFS
-            vis[src]=true;
-            list<int> neighbours = listOfIntegers[src];
-
-            for(int v : neighbours){
-                if(!vis[v]){
-
-                  if( isCycleUnderDSF(v,src,vis)){
-                        return true;
-                  }
-                   
-                }else if (v !=parent){                                                    //if cycle exists then return true.
-                    return true;
-                }
-                
-            }
-            return false;                                                                //found no cycle
-            
-}
-//to get rid of passing parent and source again and again
-bool isCycle(){
-vector <bool> vis(Vertices,false);                                                                   //V = number of vertices already has false values assigned.
-
-for(int i=0;i<Vertices;i++){
-    if(!vis[i]){
-
-        isCycleUnderDSF(i,-1,vis);                                                  // i: source , -1 : parent of source , vis : visited array
-
-    }
-}
-return false;
-}
-bool isCycleUnderBFS(int src, vector<bool> &vis){
-    
-    queue<pair<int,int>> q;
-        q.push({src,-1});
-        vis[src]=true;
-
-    while(q.size()>0){
-     
-        int u=q.front().first;
-        int parentU=q.front().second;
-        q.pop();
-
-                cout << u << " ";
-
-
-        list<int> neighbours = listOfIntegers[u];        //storing the listofintegers on my u.
-
-
-        for( int v : neighbours){
-            if(!vis[v]){
-                q.push({v,u});        //u-----------v
-                 vis[v]=true;
-            }else if(v!=parentU){    //if there exist an edge then we got a cycle
-                return true;
-            }               
-
-        }
-    }
-    return false;  //got no cycle
-
-}
-bool isCycleBFS(){          //using these method to ensure we cover the entire graph as the graphs might be disjoint and also to make the code look readable
-    vector<bool> visited(Vertices,false);
-    
-   
-    if(!visited[Vertices]){
-        if(isCycleUnderBFS(Vertices,visited)){
-            return true;
-            }
-        }
-    
-}
-
-
 };
 
 int main(){
@@ -172,9 +94,6 @@ int main(){
     cout << "DFS \n: ";
     g.DFS();
 
-   g.isCycle() ? cout << "Cycle detected using DFS \n" : cout << "No Cycle detected\n";
-
-    g.isCycleBFS() ? cout << "Cycle detected using BFS \n" : cout << "No Cycles detected\n";
 
     return 0;
 }
