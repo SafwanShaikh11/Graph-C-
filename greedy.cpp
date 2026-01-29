a8f5a58a4ea4570846cc0ca210972bf429197c1b
Safwan Shaikh <walterwhte112@gmail.com>
2026-01-29 22:50:58 +0530

    dijkstra's algorithmn

---
+++ greedy.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<queue>
+#include<climits>
+
+using namespace std;
+
+class Edge{
+public:                      //constructor
+
+int vertices;
+int weight;
+
+    Edge(int vertices, int weight){
+        this->vertices=vertices;
+        this->weight=weight;
+
+
+    }
+    
+};
+
+void dijkstra(int src,vector<vector<Edge>> g,int Vertex ){
+
+    vector<int> distance(Vertex,INT_MAX);
+
+    distance[src]=0;
+
+    priority_queue<pair<int,int>,vector<pair<int,int>>> pq;  //min-heap queue
+    //dist[u],u
+
+    pq.push({0,src});
+
+    while(pq.size()>0){
+        int u=pq.top().second;
+        pq.pop();
+
+        for(Edge e  : g[u]){ // Edge-relaxation
+
+            if(distance[e.vertices]>distance[u]+e.weight){
+
+                distance[e.vertices]=distance[u]+e.weight;
+
+                pq.push({distance[e.vertices],e.vertices});
+            }
+             
+        }
+    }
+
+    //loop for all the distance values
+    for(int i=0;i<Vertex;i++){
+        cout << distance[i] << " ";
+    }
+        cout<<endl;
+}
+
+int main(){
+
+int V=6;  //no. of vertices
+vector<vector<Edge>> g(V);  //it's a jagged array 
+
+
+g[0].push_back(Edge(1,2));  //(edge, weight)
+g[1].push_back(Edge(2,4));
+
+g[1].push_back(Edge(2,1));
+g[1].push_back(Edge(3,7));
+
+g[2].push_back(Edge(4,3));
+
+g[3].push_back(Edge(5,1));
+
+g[4].push_back(Edge(3,2));
+g[4].push_back(Edge(5,5));
+
+dijkstra(0,g,V); //stack matters 
+
+
+return 0;
+}
